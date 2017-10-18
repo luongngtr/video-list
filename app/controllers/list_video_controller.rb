375b319a5b14821bae30b56d9c5148ea4be06720
Luong Nguyen <luong.ngtr@gmail.com>
2017-10-18 04:34:23 +0000

    Second commit

--- app/controllers/list_video_controller.rb
+++ app/controllers/list_video_controller.rb
@@ -56,68 +56,119 @@ class ListVideoController < ApplicationController
     @vidchosenid = @vids.first.id
     @vidembeddedurl = @vids.find_by_id(@vidchosenid).address
     
-    if params[:video][:address] =~ URI::DEFAULT_PARSER.regexp[:ABS_URI]
-      # Correct URL
+    @is_it_successful = 'false'
+    @notification_message = 'URL is invalid or the video had been added before. \n So, nothing has been changed!'
+    
+    # Check if "videoaddress" is an embedded youtube url or not
+    # If not, convert it to an embedded one
+    videoaddress = params[:video][:address]
+    if !videoaddress.include?("embed")
+      # if "videoaddress" is not an embedded url, then
+      videoaddress = videoaddress.sub("/watch?v=", "/embed/")
+    end
+      
+    # Check if URL is correct or not
+    if (params[:video][:address] =~ URI::DEFAULT_PARSER.regexp[:ABS_URI]) && (Vid.where(:address => videoaddress).empty?) 
+      
+      # Check if there is a name for the new video, if not, then name it "Some Video"
       if params[:video][:name] == ""
         videoname = "Some Video"
       else
         videoname = params[:video][:name]
       end
       
+      # Check if there is a description for the new video, if not, then add it "Coming Soon"
       if params[:video][:description] == ""
         videodescription = "Comming Soon"
       else
         videodescription = params[:video][:description]
       end
-      videoaddress = params[:video][:address]
       
-      # Check if "videoaddress" is an embedded youtube url or not
-      # If not, convert it to an embedded one
-      if !videoaddress.include?("embed")
-        # if "videoaddress" is not an embedded url, then
-        videoaddress = videoaddress.sub("/watch?v=", "/embed/")
-      end
       
+      # The input type of the new video may be a collection of names or nil
       videotypes = params[:video][:checkedtype]
       
       # When the new video has only a new type
       # or no type is checked
       if videotypes.nil?
+        videotypes = []
         if params[:video][:newtype] == ""
-          videotypes = "Others"
+          videotypes << "Others"
+          # Create a new type if it does not exist
+          if Type.find_by_name("Others").nil?
+            Type.create(name: "Others")
+            @notification_message = 'The new video has been added successfully! \n However, no type is chosen \n The new video is now in the list of Others'
+          end
         else
-          videotypes = params[:video][:newtype]
-        end
-        # Create a new type if it does not exist
-        if Type.find_by_name(videotypes).nil?
-          Type.create(name: videotypes)
+          videotypes << params[:video][:newtype]
+          # Create a new type if it does not exist
+          if Type.find_by_name(params[:video][:newtype]).nil?
+            is_there_others = 'false'
+            if !Type.find_by_name("Others").nil?
+              Type.find_by_name("Others").delete
+              is_there_others = 'true'
+            end
+            Type.create(name: params[:video][:newtype])
+            @notification_message = 'The new video has been added successfully! \n We also has a new type of videos!'
+            if is_there_others
+              Type.create(name: "Others")
+            end
+          end
         end
       end
       
-      # When the new video has several types including a new type
-      if params[:video][:newtype] == ""
-        videotypes[-1] = "Others"
-        if Type.find_by_name(videotypes[-1]).nil?
-          Type.create(name: videotypes[-1])
-        end
-      else
-        videotypes[-1] = params[:video][:newtype]
-        if Type.find_by_name(videotypes[-1]).nil?
-          Type.create(name: videotypes[-1])
+      # When the new video has several types
+      if !videotypes.nil?
+        newtype = params[:video][:newtype]
+        if newtype != ""
+          if newtype != "Others"
+            if Type.find_by_name(newtype).nil?
+              is_there_others = 'false'
+              if !Type.find_by_name("Others").nil?
+                Type.find_by_name("Others").delete
+                is_there_others = 'true'
+              end
+              Type.create(name: newtype)
+              @notification_message = 'The new video has been added successfully! \n We also has a new type of videos!'
+              if is_there_others
+                Type.create(name: "Others")
+              end
+              videotypes << newtype
+            end
+          else
+            if Type.find_by_name("Others").nil?
+              Type.create(name: "Others")
+              @notification_message = 'The new video has been added successfully! \n We also has a new type of videos!'
+            else
+              @notification_message = 'The new video has been added successfully! \n However, no new type has been added!'
+            end
+          end
         end
       end
       
       # Append videotypes with "All"
       videotypes.push("All")
-      
-      # Add a new video if it does not exist
-      if Vid.where(:address => videoaddress).empty?   
-         Vid.create(name: videoname, address: videoaddress, 
-           description: videodescription, :types => Type.where(:name => videotypes))
+     
+        
+      # Delete "Others" first (and add later to make sure that "Others" is always the last)
+      is_there_unknown_video = 'false'
+      if !Vid.find_by_name("Unknown Video").nil?
+       Vid.find_by_name("Unknown Video").delete
+       is_there_unknown_video = 'true'
       end
-      # Vid.create(name: videoname, address: videoaddress, 
-      #    description: videodescription, :types => Type.where(:name => videotypes))
+       
+      Vid.create(name: videoname, address: videoaddress, 
+       description: videodescription, :types => Type.where(:name => videotypes))
+         
+      if is_there_unknown_video
+       Vid.create(name: "Unknown Video", address: "https://www.youtube.com/embed/KkmNBWEfb24?ecver=2", 
+         description: "Comming Soon")
+       Vid.find_by_name("Unknown Video").types << Type.all
+      end
       
+      @is_it_successful = 'true'
+    
+       
       # The new video should be displayed immidiately with its first type
       @types = Type.all
       @typesexceptalls = @types[1..-1]
@@ -127,6 +178,7 @@ class ListVideoController < ApplicationController
       @vidchosenid = @vids.find_by_name(videoname).id
       
       @vidembeddedurl = @vids.find_by_id(@vidchosenid).address
+      
       
     end
     
